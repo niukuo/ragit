22917adee8e2705e4cba8710d7cf26ec21045b80
yiteng.nyt <yiteng.nyt@alibaba-inc.com>
2019-09-03 12:02:54 +0800

    ragit: fix slow log
    Change-Id: I39797da0228e4657b00d440484fbec8020c9c113

--- raft/message.go
+++ raft/message.go
@@ -7,6 +7,43 @@ import (
 	pb "go.etcd.io/etcd/raft/raftpb"
 )
 
+type Entry pb.Entry
+
+func (e *Entry) String() string {
+	if e == nil {
+		return "<nil>"
+	}
+	content := make(map[string]interface{})
+	if e.Term != 0 {
+		content["Term"] = e.Term
+	}
+	if e.Index != 0 {
+		content["Index"] = e.Index
+	}
+	content["Type"] = e.Type
+	dataStr := ""
+	data := e.Data
+	maxLen := 128
+	if len(data) > maxLen {
+		data = data[:maxLen]
+		dataStr = fmt.Sprintf("(%d/%d bytes)", len(data), len(e.Data)) + string(data)
+	} else {
+		dataStr = fmt.Sprintf("(%d bytes)", len(e.Data)) + string(data)
+	}
+	content["data"] = dataStr
+	return fmt.Sprintf("%v", content)
+}
+
+type Entries []pb.Entry
+
+func (es Entries) String() string {
+	s := make([]*Entry, 0, len(es))
+	for i := range es {
+		s = append(s, (*Entry)(&es[i]))
+	}
+	return fmt.Sprintf("%v", s)
+}
+
 type Message pb.Message
 
 func (m *Message) String() string {
@@ -28,7 +65,7 @@ func (m *Message) String() string {
 		content["Commit"] = m.Commit
 	}
 	if len(m.Entries) > 0 {
-		content["entries"] = m.Entries
+		content["entries"] = Entries(m.Entries)
 	}
 	if !raft.IsEmptySnap(m.Snapshot) {
 		content["snapshot"] = m.Snapshot
